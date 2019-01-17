24078c49dda7b5f67f60d87c0bdf7fb5705611cf
Brad McKinney <brad.mckin@gmail.com>
2019-01-17 09:39:23 -1000

    generates a sorted matrix based on user input

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,23 @@
 function matrixGenerator(rows, columns) {
-  // do work here
+  const matrix = [];
+  for (let i = 0; i < rows; i++) {
+    let row = [];
+
+    for (let x = 0; x < columns; x++) {
+      let column = Math.floor((Math.random() * 100)) + 1;
+      row.push(column);
+    }
+
+    row.sort((a, b) => {
+      return a - b;
+    });
+    matrix.push(row);
+  }
+
+  matrix.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  return matrix;
 }
 
 module.exports = matrixGenerator;
